b998111ea05eccb2e3b29035ff2f24aa3a403a18
Andy Javier <andy_javier@ucne.edu.do>
2024-11-04 12:25:30 -0400

    moví de un lado a otro las cosas

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Tecnicos.Data.Context;
+using Tecnicos.Services.DI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Inject Context
+builder.Services.RegisterServices();
+
 
 var app = builder.Build();
 
